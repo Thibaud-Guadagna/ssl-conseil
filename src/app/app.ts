c2758bc708be2b2b5b8763cb87d7c1938579317f
thibaud-guadagna <thibaud.guadagna@gmail.com>
2025-09-01 11:24:50 +0200

    Add Prestations

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from "@angular/core";
 import { Navbar } from "./components/navbar/navbar";
+import { Header } from "./components/header/header";
+import { Prestations } from "./components/prestations/prestations";
 
 @Component({
 	selector: "app-root",
-	imports: [Navbar],
+	imports: [Navbar,Header,Prestations],
 	standalone: true,
 	templateUrl: "./app.html",
 	styleUrl: "./app.css",
